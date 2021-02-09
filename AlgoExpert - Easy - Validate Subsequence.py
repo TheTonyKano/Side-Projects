d7b57ca6980d6a17573a1799eb17db48be8f9b2d
TheTonyKano <70045901+TheTonyKano@users.noreply.github.com>
2021-02-08 21:32:09 -0500

    Delete AlgoExpert - Easy - Validate Subsequence.py

--- AlgoExpert - Easy - Validate Subsequence.py
+++
@@ -1,8 +0,0 @@
-def isValidSubsequence(array, sequence):
-	posSeq = 0
-	for i in array:
-			if posSeq == len(sequence):
-					break
-			if sequence[posSeq] == i:
-					posSeq += 1
-	return posSeq == len(sequence)
