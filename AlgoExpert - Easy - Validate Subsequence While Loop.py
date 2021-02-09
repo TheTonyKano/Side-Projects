fc9aaa5e3c3e5fec3e823ea84793a83c8cc24073
TheTonyKano <70045901+TheTonyKano@users.noreply.github.com>
2021-02-08 21:32:19 -0500

    Delete AlgoExpert - Easy - Validate Subsequence While Loop.py

--- AlgoExpert - Easy - Validate Subsequence While Loop.py
+++
@@ -1,8 +0,0 @@
-def isValidSubsequence(array, sequence):
-    posA = 0
-    posS = 0
-    while posA < len(array) and posS < len(sequence):
-        if array[posA] == sequence[posS]:
-            posS+=1
-        posA += 1
-    return posS == len(sequence)
